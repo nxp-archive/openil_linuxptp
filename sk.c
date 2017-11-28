e98cec1b397bbd1773b105f13db2fbb1516fc0db
Yangbo Lu <yangbo.lu@nxp.com>
2017-11-28 18:11:48 +0800

    sja1105-ptp: add more debug message
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- sk.c
+++ sk.c
@@ -288,18 +288,23 @@ void ptp_insert_correction(struct ptp_message *m)
 
 	switch (msg_type(m)) {
 	case FOLLOW_UP:
-		if (!clock->interface->sync)
+		if (!clock->interface->sync) {
+			printf("follow_up: didn't have a sync msg stored!\n");
 			return;
+		}
 
 		if (clock->interface->sync->header.sequenceId ==
 					ntohs(m->header.sequenceId)) {
 			m->header.correction = sync_tx_ts.tx_ts;
 			m->header.correction = host2net64(m->header.correction);
-		}
+		} else
+			printf("follow_up: didn't match with sync msg stored!\n");
 		break;
 	case DELAY_RESP:
-		if (!clock->interface->delay_req)
+		if (!clock->interface->delay_req) {
+			printf("delay_resp: didn't have a delay_req msg stored!\n");
 			return;
+		}
 
 		req = &clock->interface->delay_req->delay_req;
 
@@ -308,10 +313,14 @@ void ptp_insert_correction(struct ptp_message *m)
 		rsp_tmp.requestingPortIdentity.portNumber = ntohs(rsp_tmp.requestingPortIdentity.portNumber);
 
 		if (memcmp(&rsp_tmp.requestingPortIdentity, &req->hdr.sourcePortIdentity,
-				sizeof(rsp_tmp.requestingPortIdentity)))
+				sizeof(rsp_tmp.requestingPortIdentity))) {
+			printf("delay_resp: port id didn't match with delay_req\n");
 			return;
-		if (ntohs(rsp_tmp.hdr.sequenceId) != req->hdr.sequenceId)
+		}
+		if (ntohs(rsp_tmp.hdr.sequenceId) != req->hdr.sequenceId) {
+			printf("delay_resp: seq id didn't match with delay_req\n");
 			return;
+		}
 
 		m->header.correction = delay_req_tx_ts.tx_ts;
 		m->header.correction = host2net64(m->header.correction);
